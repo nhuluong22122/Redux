6e6de006db116aef0d39ee0f16b2f113aba9467d
Nhu Luong <nhuluong2212@gmail.com>
2018-01-15 22:02:26 -0800

    Set up Redux environment & reducers

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+//A reducer that other files in the stack can use
+export default function () {
+  return [
+    { title: 'The Defining Decade' , pages: 234 },
+    { title: 'When Breath Becomes Air', pages: 120 },
+    { title: 'Give and Take', pages: 340 },
+    { title: 'True Face', pages: 80 }
+  ];
+}
